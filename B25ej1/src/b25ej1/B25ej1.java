176564d1d250faada9129330b0d6bc9523132219
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-04-17 09:17:20 +0200

    Boletín 25 de programacion. Bonoloto

---
+++ B25ej1/src/b25ej1/B25ej1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b25ej1;
+
+/**
+ *
+ * @author quique
+ */
+public class B25ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bonoloto bon  = new Bonoloto();
+        
+        bon.setVisible(true);
+    }
+    
+}

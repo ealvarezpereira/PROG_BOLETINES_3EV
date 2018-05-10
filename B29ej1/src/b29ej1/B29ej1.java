c57e17a691df3cf13abf0f4988572138462b2eb9
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-05-10 13:56:03 +0200

    Boletin 29 de programacion. Polimorfismo.

--- B29ej1/src/b29ej1/B29ej1.java
+++ B29ej1/src/b29ej1/B29ej1.java
@@ -18,29 +18,20 @@ public class B29ej1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         List<Barco> barcos = new ArrayList();
-        
-        Barco v = new Velero(2,35.2f,"YASS34",7);
-        Barco d = new Deportivo(150,15.5f,"ATOPE23",10);
-        Barco y = new Yate(7740,5,44f,"MONEY69",1);
-        
+
+        Barco v = new Velero(2, 35.2f, "YASS34", 7);
+        Barco d = new Deportivo(150, 15.5f, "ATOPE23", 10);
+        Barco y = new Yate(7740, 5, 44f, "MONEY69", 1);
+
         barcos.add(v);
         barcos.add(d);
         barcos.add(y);
-        
+
         for (Barco bfor : barcos) {
-            
             bfor.aluguer(bfor);
         }
-        
-        // TODO code application logic here
-        
-        
-        //mastiles * 2
-        //potencia * 3
-        //camarotes * 4
-        
     }
-    
+
 }
